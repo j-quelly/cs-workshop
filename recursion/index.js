2590a12c010ff71817d91b9c302f4c4976d99142
Jamie Kelly <jkelly@tesla.com>
2018-03-25 01:23:48 -0700

    [UPDATE] recursion: fibonacci & factorial 3

--- recursion/index.js
+++ recursion/index.js
@@ -1,6 +1,7 @@
 /*
   Make a function that computes a factorial recursively.
-  A factorial is when you take a number n and multiply by each preceding integer until you hit one.
+  A factorial is when you take a number n and multiply by each preceding
+  integer until you hit one.
   n * (n-1) * (n-2) ... * 3 * 2 * 1
 
   Call the function factorial
@@ -10,6 +11,12 @@
   factorial(3) = 6
 */
 
-module.export = function factorial(n) {
+module.exports = function factorial(n) {
+  if (n < 2) {
+    return n;
+  }
 
+  // 3 * (3 - 1)
+  // base case
+  return n * factorial(n - 1);
 };
--- recursion/recursion.analysis.js
+++ recursion/recursion.analysis.js
@@ -3,7 +3,7 @@ const factorial = require('./index.js');
 // unit tests
 // do not modify the below code
 describe('factorial', () => {
-  xit('should do factorials', () => {
+  it('should do factorials', () => {
     expect(factorial(1)).toEqual(1);
     expect(factorial(2)).toEqual(2);
     expect(factorial(3)).toEqual(6);
